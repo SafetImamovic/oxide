ec771c29834ea005ebef770075a5ae41894c36ed
SafetImamovic <safet.imamovic.22@size.ba>
2025-08-28 20:14:03 +0200

    ci(site): Site deployment.

--- docs/oxide_bg.wasm.d.ts
+++ docs/oxide_bg.wasm.d.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const run_web: () => void;
+export const run_oxide_wasm: () => void;
 export const __externref_table_alloc: () => number;
 export const __wbindgen_export_1: WebAssembly.Table;
 export const __wbindgen_exn_store: (a: number) => void;
@@ -9,8 +9,8 @@ export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
 export const __wbindgen_free: (a: number, b: number, c: number) => void;
 export const __wbindgen_export_6: WebAssembly.Table;
-export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7de2e63149679350: (a: number, b: number) => void;
-export const closure3084_externref_shim: (a: number, b: number, c: any) => void;
-export const closure3089_externref_shim: (a: number, b: number, c: any, d: any) => void;
-export const closure3261_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1906bbbc873e7667: (a: number, b: number) => void;
+export const closure2872_externref_shim: (a: number, b: number, c: any) => void;
+export const closure2874_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure3046_externref_shim: (a: number, b: number, c: any) => void;
 export const __wbindgen_start: () => void;
