074ff3fedae0ff3be11f43c0761334f9ba15b50c
SafetImamovic <safet.imamovic.22@size.ba>
2025-09-02 13:29:25 +0200

    feat(deployment): Add easier way to deploy all examples.

---
+++ crates/snake/src/main.rs
@@ -0,0 +1,9 @@
+//#[engine_start] // To be proc_macro
+fn engine_start() {}
+
+fn main()
+{
+        oxide::utils::bootstrap::config_logging();
+
+        //log::info!("Snake");
+}
--- wasm-stage.sh
+++ wasm-stage.sh
@@ -1,18 +1,29 @@
-#!/usr/bin/env bash
+# instructions:
+# starting from root
+# go to -> crates/ 
+# go through all of the directories within crates like:
+# 	oxide/
+# 	snake/
+#
+# next run this command within each directory:
+# set -euo pipefail
 
-# safer script (exit on error, unset vars, and pipe errors).
-set -euo pipefail
-
-RUSTFLAGS='--cfg getrandom_backend="wasm_js"' \
-    wasm-pack build --target web --no-default-features
-
-rm -rf ./docs
-
-mkdir -p ./docs
-
-# Copy everything from pkg to docs, excluding .gitignore
-shopt -s extglob
-
-cp -r ./pkg/!(.gitignore) ./docs/
-
-shopt -u extglob
+# RUSTFLAGS='--cfg getrandom_backend="wasm_js"' \
+#    wasm-pack build --target web --no-default-features	
+#
+# Next you'll get something like this:
+# 	oxide/
+# 		pkg/
+# 	snake/
+# 		pkg/
+#
+# Next we want to move these pkg build files into to root docs/ foler for github page deployment.
+#
+# But we have to rename them first from pkg to their appropriate name like pkg -> oxide, pkg -> snake
+#
+# we should get something like this:
+# docs/
+# 	index.html # this is always here
+# 	oxide/
+# 	snake/
+# 	...
